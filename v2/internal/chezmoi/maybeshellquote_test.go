a6e274a705d249b59ff0374046c9bb79090a180c
Tom Payne <twpayne@gmail.com>
2020-05-14 23:13:26 +0100

    snapshot

--- v2/internal/chezmoi/maybeshellquote_test.go
+++ v2/internal/chezmoi/maybeshellquote_test.go
@@ -24,3 +24,25 @@ func TestMaybeShellQuote(t *testing.T) {
 		assert.Equal(t, expected, MaybeShellQuote(s), "quoting %q", s)
 	}
 }
+
+func TestShellQuoteArgs(t *testing.T) {
+	for _, tc := range []struct {
+		args     []string
+		expected string
+	}{
+		{
+			args:     []string{},
+			expected: "",
+		},
+		{
+			args:     []string{"foo"},
+			expected: "foo",
+		},
+		{
+			args:     []string{"foo", "bar baz"},
+			expected: "foo 'bar baz'",
+		},
+	} {
+		assert.Equal(t, tc.expected, ShellQuoteArgs(tc.args))
+	}
+}
